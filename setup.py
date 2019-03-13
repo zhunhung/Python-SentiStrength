cd6d6362cfa46fedfab9a015f5a3beaf91e3ae95
zhunhung <yongzhunhung@gmail.com>
2019-03-13 23:34:07 +0800

    Updated Setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="sentistrength-zhunhung",
+    name="sentistrength",
     version="0.0.1",
     author="Zhun Hung",
     author_email="yongzhunhung@gmail.com",
